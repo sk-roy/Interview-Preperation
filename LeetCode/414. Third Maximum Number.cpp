83ca036ec04dd76d5c357e036b20ebb8ec9eeadd
Sujit Kumar Roy <sujitkumarroyy@gmail.com>
2022-09-08 12:08:52 +0600

    Solve leetcode problem 414. Third Maximum Number with time complexity O(logn)

--- LeetCode/414. Third Maximum Number.cpp
+++ LeetCode/414. Third Maximum Number.cpp
@@ -5,18 +5,54 @@ public:
 
     int thirdMax(vector<int> &nums)
     {
-        sort(nums.begin(), nums.end(), comp);
-        int count = 1;
 
-        for (int i = 1; i < nums.size(); i++)
+        bool got1 = false;
+        bool got2 = false;
+        bool got3 = false;
+        int maxi1, maxi2, maxi3;
+
+        for (auto num : nums)
         {
-            if (nums[i - 1] != nums[i])
+            if (got1)
+                maxi1 = max(maxi1, num);
+            else
             {
-                count++;
-                if (count == 3)
-                    return nums[i];
+                maxi1 = num;
+                got1 = true;
             }
         }
-        return nums[0];
+
+        for (auto num : nums)
+        {
+            if (num != maxi1)
+            {
+                if (got2)
+                    maxi2 = max(maxi2, num);
+                else
+                {
+                    maxi2 = num;
+                    got2 = true;
+                }
+            }
+        }
+        for (auto num : nums)
+        {
+            if (num != maxi1 and num != maxi2)
+            {
+                if (got3)
+                    maxi3 = max(maxi3, num);
+                else
+                {
+                    maxi3 = num;
+                    got3 = true;
+                }
+            }
+        }
+        // cout<<INT_MIN<<endl;
+
+        if (got3)
+            return maxi3;
+        else
+            return maxi1;
     }
 };
