571b22c11e0483274ba5cc4d12aee118cd034b37
Sujit Kumar Roy <sujitkumarroyy@gmail.com>
2022-09-13 23:38:05 +0600

    119. Pascal's Triangle II: Modify solution to O(rowIndex) extra space

--- LeetCode/119. Pascal's Triangle II.cpp
+++ LeetCode/119. Pascal's Triangle II.cpp
@@ -1,25 +1,21 @@
 class Solution {
 public:
     vector<int> getRow(int rowIndex) {
-        int N  = rowIndex + 2;
-        int nCr[N][N];
+        rowIndex++;
+        vector <int> pascal(rowIndex);
 
-        for (int i = 0; i < N; i++) {
-            nCr[i][0] = 1;
-            nCr[i][i] = 1;
+        for (int i = 0; i < rowIndex; i++) {
+            pascal[i] = 0;
         }
 
-        for (int i = 2; i <= rowIndex; i++) {
-            for (int j = 1; j < i; j++) {
-                nCr[i][j] = nCr[i-1][j-1] + nCr[i-1][j];
+        pascal[0] = 1;
+
+        for (int i = 1; i < rowIndex; i++) {
+            for (int j = i; j > 0; j--) {
+                pascal[j] += pascal[j-1];
             }
         }
 
-        vector <int> v;
-        for (int i = 0; i <= rowIndex; i++) {
-            v.push_back(nCr[rowIndex][i]);
-        }
-
-        return v;
+        return pascal;
     }
 };
