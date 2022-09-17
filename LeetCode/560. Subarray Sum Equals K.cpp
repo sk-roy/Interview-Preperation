f345434cd554ecd27b83b339fa4b06dedcb57c49
Sujit Kumar Roy <sujitkumarroyy@gmail.com>
2022-09-17 09:49:09 +0600

    Update and solve following leetcode problems # 560. Subarray Sum Equals K (Optimize solution using hashmap) # 706. Design HashMap

--- LeetCode/560. Subarray Sum Equals K.cpp
+++ LeetCode/560. Subarray Sum Equals K.cpp
@@ -3,23 +3,18 @@ public:
     int subarraySum(vector<int>& nums, int k) {
         int sum = 0;
         int counter = 0;
-        vector <int> sm = nums;
+        unordered_map <int, int> mp;
+        mp[sum] = 1;
 
-        for (int i = 1; i < sm.size(); i++) {
-            sm[i] += sm[i-1];
-        }
+        for (auto num: nums) {
+            sum += num;
+            int diff = sum - k;
 
-        for (int i = 0; i < nums.size(); i++) {
-            for (int j = i; j < nums.size(); j++) {
-                sum = sm[j];
-                if (i != 0) {
-                    sum -= sm[i-1];
-                }
-
-                if (sum == k) {
-                    counter++;
-                }
+            if (mp.find(diff) != mp.end()) {
+                counter += mp[diff];
             }
+
+            mp[sum]++;
         }
 
         return counter;
